22db03b231fdd372d7f4c7cfa5c7c3d65471ea41
PhilimonElkessDawoud <P.E.DAOUD@student.aast.edu>
2026-05-19 17:32:40 +0200

    Project completed!

---
+++ src/app/pages/task-form/task-form.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { TaskService } from '../../services/task-service';
+
+@Component({
+  selector: 'app-task-form',
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './task-form.html',
+  styleUrl: './task-form.css',
+})
+export class TaskForm {
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+  private taskService = inject(TaskService);
+
+  taskForm = this.fb.group({
+    title: ['', [Validators.required, Validators.minLength(3)]],
+    description: ['', [Validators.required, Validators.minLength(10)]]
+  });
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      const { title, description } = this.taskForm.value;
+      if (title && description) {
+        this.taskService.addTask(title, description);
+        this.taskForm.reset();
+        this.router.navigate(['/']);
+      }
+
+    }
+  }
+}
---
+++ src/app/services/task-service.ts
@@ -0,0 +1,70 @@
+import { computed, Injectable, signal } from '@angular/core';
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: Date;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private tasksSignal = signal<Task[]>([
+    {
+      id: 1,
+      title: 'Learn Angular Basics',
+      description: 'Understand components, services, and routing',
+      completed: true,
+      createdAt: new Date('2026-05-19'),
+    },
+    {
+      id: 2,
+      title: 'Build a Project',
+      description: 'Create a task manager application',
+      completed: false,
+      createdAt: new Date('2025-06-20'),
+    },
+    {
+      id: 3,
+      title: 'Build a Project',
+      description: 'Create a user manager application',
+      completed: false,
+      createdAt: new Date('2025-07-23'),
+    },
+  ]);
+
+  tasks = this.tasksSignal.asReadonly();
+
+  completedTasks = computed(() => {
+    return this.tasksSignal().filter((tasks) => tasks.completed);
+  });
+
+  activeTasks = computed(() => {
+    return this.tasksSignal().filter((tasks) => !tasks.completed)
+  })
+
+  getTask(id: number) {
+    return this.tasks().find(task => task.id === id)
+  }
+
+  addTask(title: string, description: string) {
+    const task: Task = {
+      id: this.tasks().length + 1,
+      title,
+      description,
+      completed: false,
+      createdAt: new Date()
+    };
+
+    this.tasksSignal.update((tasks) => [...tasks, task])
+  }
+
+  deleteTask(id: number) {
+    this.tasksSignal.update((tasks) => {
+      return tasks.filter((task) => task.id !== id);
+    });
+  }
+}
